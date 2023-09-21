ff59394c1c108371f3221a372d09a2a7dde3d340
Shivanikshivi <97271307+Shivanikshivi@users.noreply.github.com>
2023-09-21 12:39:45 +0530

    Factorial

---
+++ factorial.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class factorial {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int i,fact=1;
+    int num=5;
+    for(i=1;i<num;i++)
+    {
+    	fact = fact*i;
+    
+	}
+System.out.println("the factorial of" +num+ "is:" +fact);
+}
+}
