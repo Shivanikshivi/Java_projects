30b7b7428ae1b7220b7cfa36146211ae9956dbaf
Shivanikshivi <97271307+Shivanikshivi@users.noreply.github.com>
2023-09-25 12:23:19 +0530

    first commit

---
+++ Alphabets.java
@@ -0,0 +1,12 @@
+package basics;
+
+public class Alphabets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch;
+	      for(ch = 'A'; ch <= 'Z'; ++ch)
+	      System.out.print(ch + " ");
+	}
+
+}
