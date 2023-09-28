a6f71cc71faa5df83a1d3abee6b8f15c12103480
Shivanikshivi <97271307+Shivanikshivi@users.noreply.github.com>
2023-09-28 11:02:00 +0530

    first commit

---
+++ AccessModifiers.java
@@ -0,0 +1,15 @@
+/* 1. Four access modifiers available in java are private public protected and default 
+Private - visibility inside the same class, i.e, it can be accessed only from the particular class. It restricts access to members within the same class.
+Public - When a variable is public it can be accessed throughout the project. 
+Protected - The members can be accessed within the same package and also subclasses to access them.
+Default - All members are visible within the same package but are not accessible by other packages.
+
+2. Exceptions only occur at run time but errors occur both at compile time and run time.
+   There are many ways to handle exceptions, one method is try catch block, where it catches and handle exceptions. In teh try block we will write the code that might throw exception and in the catch block the thrown exception will be caught.
+   If the exception is not removed from the program, it can result in program termination. 
+
+3. Difference between checked and unchecked exception 
+   Checked exception is also known as compile time exception, where the source code is transformed to executable code.
+   and unchecked execution is also called run time execution and it is done in the run time when the program is in execution. It will be thrown only when there is a error in runtime which we won't be able to predict. If any exception is thrown it will come out of the program abruptly.'
+   Finally keyword is used in checked exceptions where it compulsarily gets executed when there is an exception, io exception is a compile time/checked exception. */
+
