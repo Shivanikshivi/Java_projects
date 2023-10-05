1ed4166761a91d7131239c79d7cf04ceb6d05ca3
Shivanikshivi <97271307+Shivanikshivi@users.noreply.github.com>
2023-10-05 12:55:11 +0530

    first commit

--- Employee.java
+++ Employee.java
@@ -1,31 +1,53 @@
-package com.example.hr;
+package advance;
+public class Employee 
+{
+    private String name;
+    private int id;
+    private double salary;  
+    public Employee(String name, int id, double salary) 
+    {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
 
-public class Employee {
+      public String getName() 
+      {
+        return name;
+      }
 
-		private String name;
-	    private int id;
-	    private double salary;
+       public int getId() 
+       {
+        return id;
+       }
 
-	    public Employee(String name, int id, double salary) 
-	    {
-	        this.name = name;
-	        this.id = id;
-	        this.salary = salary;
-	    }
-
-	    public void printName() 
-	    {
-	        System.out.println("Employee Name: " + name);
-	    }
-
-	    public void printSalary() 
-	    {
-	        System.out.println("Employee Salary: Rs." + salary);
-	    
-	}
-}
     
-	
+    public double getSalary() 
+    {
+        return salary;
+    }
 
+    
+    public void raiseSalary(double amount) 
+    {
+        if (amount >= 0) 
+        {
+            salary += amount;
+            System.out.println(name + "'s salary has been raised by $" + amount);
+        } 
+        else 
+        {
+            System.out.println("Invalid raise amount. Salary remains unchanged.");
+        }
+    }
 
+    public static void main(String[] args) 
+    {
+       
+        Employee employee = new Employee("Adhi", 503, 50000.0);      
+        System.out.println("Initial Salary: $" + employee.getSalary());
+        employee.raiseSalary(2000.0);       
+        System.out.println("Updated Salary: $" + employee.getSalary());
+    }
+}
 
