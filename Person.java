61fbede32d3e042c6410a7415a6cd7c4a5d1ce2b
Shivanikshivi <97271307+Shivanikshivi@users.noreply.github.com>
2023-10-05 12:35:49 +0530

    first commit

---
+++ Person.java
@@ -0,0 +1,29 @@
+package advance;
+
+public class Person {
+    private String name;
+    private int age;
+
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+       
+        Person person = new Person("Adhi", 34);
+        System.out.println("Name: " + person.getName());
+        System.out.println("Age: " + person.getAge());
+    }
+}
